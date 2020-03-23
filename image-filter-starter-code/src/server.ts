3f31685face41dee2dc707d63252addc0efe29a8
Christodoulou <petrochr@a483e7b03a2d.ant.amazon.com>
2020-03-23 10:21:15 +0000

    all code

---
+++ image-filter-starter-code/src/server.ts
@@ -0,0 +1,56 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { filterImageFromURL, deleteLocalFiles } from "./util/util";
+
+(async () => {
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  // Get a signed url to put a new item in the bucket
+  app.get("/filteredimage", async (req, res) => {
+    // Access the provided 'page' and 'limt' query parameters
+    console.log("QUERY ", req.query);
+    let image_url = req.query.image_url;
+    if (!image_url) {
+      return res.status(401).send({ message: "Image URL is required" });
+    }
+    let file = await filterImageFromURL(image_url);
+    res.status(200).sendFile(file);
+  });
+
+  //! END @TODO1
+
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}");
+  });
+
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
+})();
